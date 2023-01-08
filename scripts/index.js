5864a3d27892dbd782e91acaba6443270578692a
gnt21 <gnt21@hotmail.com>
2023-01-08 01:39:49 -0300

    ajuste copyright

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,6 @@ const modified = new Date(document.lastModified);
 const fulldate = `Last Updated: ${("0"+modified.getMonth()+1).slice(-2)}/${("0"+modified.getDate()).slice(-2)}/${modified.getFullYear()}
 ${("0"+modified.getHours()).slice(-2)}:${("0"+modified.getMinutes()).slice(-2)}:${("0"+modified.getSeconds()).slice(-2)}`;
 
-document.querySelector("#name-footer").textContent = footer_name;
+document.querySelector("#name-footer").textContent = document.querySelector("#name-footer").textContent + footer_name;
 document.querySelector("#date2").textContent = fulldate;
 
