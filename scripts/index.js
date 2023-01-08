e96481d8486c2c4f79ae05f81e11b613d13f2ebb
gnt21 <gnt21@hotmail.com>
2023-01-08 01:30:52 -0300

    ajuste js

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,11 @@
+const current_year = new Date();
+const footer_name = `${current_year.getFullYear()} | José Meirelles | SP`;
 
-const d = new Date();
-const fulldate = `Last Updated: ${("0"+d.getMonth()+1).slice(-2)}/${("0"+d.getDate()).slice(-2)}/${d.getFullYear()}
-${("0"+d.getHours()).slice(-2)}:${("0"+d.getMinutes()).slice(-2)}:${("0"+d.getSeconds()).slice(-2)}`;
+
+const modified = new Date(document.lastModified);
+const fulldate = `Last Updated: ${("0"+modified.getMonth()+1).slice(-2)}/${("0"+modified.getDate()).slice(-2)}/${modified.getFullYear()}
+${("0"+modified.getHours()).slice(-2)}:${("0"+modified.getMinutes()).slice(-2)}:${("0"+modified.getSeconds()).slice(-2)}`;
+
+document.querySelector("#name-footer").textContent = footer_name;
 document.querySelector("#date2").textContent = fulldate;
+
